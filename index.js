2aa70789eff207482323893f80d27829c2558060
Cristian <64754435+CristianLich@users.noreply.github.com>
2024-07-02 17:54:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const sequelize = require('./config/database');
 const createDatabase = require('./config/initDB')
-
+//
 //Rutas
 const JuegoRoutes = require('./routes/JuegoRoutes');
 const CompraRoutes = require('./routes/CompraRoutes');
